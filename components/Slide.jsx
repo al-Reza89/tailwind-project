5489da040eb215c57ee001d94f6493cda1680234
al-Reza89 <alialreza98@gmail.com>
2022-09-22 02:35:40 +0600

    slide component done

--- components/Slide.jsx
+++ components/Slide.jsx
@@ -1,24 +1,33 @@
 import Image from "next/image";
 import React from "react";
 import chalk from "../public/assets/chalk.png";
+import { GoChevronLeft, GoChevronRight } from "react-icons/go";
 
 const Slide = () => {
   return (
-    <div className="w-full bg-blue-500 ">
-      <div className="max-w-[95%] bg-red-400 mx-auto flex">
-        <div className="w-[5%] inline-flex items-center ">A</div>
-        <div className="w-[90%]  my-5 grid grid-cols-3  ">
-          <div className="col-span-1">
+    <div className="w-full h-auto">
+      <div className="max-w-[95%] mx-auto flex">
+        <div className="w-[5%] inline-flex items-center ">
+          <GoChevronLeft className="h-14 w-14 " />
+        </div>
+        <div className="w-[90%]  my-3 grid grid-cols-3 gap-3 pl-4 ">
+          <div className="col-span-1 hidden xs:inline-grid ">
             <Image src={chalk} alt="" objectFit="contain" />
           </div>
-          <div className="col-span-2">
-            <h1>Conversations with inspiring MIT faculty</h1>
-            <h1>about how they teach.</h1>
+          <div className="xs:col-span-2 col-span-3 flex flex-col">
+            <h1 className="text-2xl font-bold ">
+              Conversations with inspiring MIT faculty
+            </h1>
+            <h1 className="text-2xl font-bold ">about how they teach.</h1>
 
-            <h1>Listen now.</h1>
+            <h1 className="text-lg underline underline-offset-2 py-4">
+              Listen now.
+            </h1>
           </div>
         </div>
-        <div className="w-[5%] inline-flex items-center justify-end ">A</div>
+        <div className="w-[5%] inline-flex items-center justify-end ">
+          <GoChevronRight className="h-14 w-14 " />
+        </div>
       </div>
     </div>
   );
