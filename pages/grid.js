6eeddb4f24e28795020c448c98be06b0cdd240e7
al-Reza89 <alialreza98@gmail.com>
2022-09-26 03:41:22 +0600

    all about col-span-m and col-start-n

--- pages/grid.js
+++ pages/grid.js
@@ -2,12 +2,15 @@ import React from "react";
 
 const grid = () => {
   return (
-    <div className="grid place-items-center min-h-screen ">
-      <div className="grid gap-4 p-4 max-w-5xl xms:grid-cols-2 ">
-        <h1 className="text-4xl font-extrabold xms:col-span-2 xms:w-1/2 ">
+    <div className="grid place-items-center min-h-screen  ">
+      <div className="grid gap-4 p-4 max-w-5xl xms:grid-cols-2 md:grid-cols-4 ">
+        <h1 className="text-4xl font-extrabold xms:col-span-2 xms:w-1/2  md:col-span-3 md:w-2/3">
           Lorem ipsum dolor sit amet consectetur.
         </h1>
-        <p className="xms:row-start-2 xms:col-start-2 self-center ">
+        <p
+          className="xms:row-start-2 xms:col-start-2 self-center md:col-start-1 
+        md:col-span-2  "
+        >
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Est alias
           error illum id, dolore consequatur voluptatibus nisi autem fuga vel
           iure aperiam consectetur laudantium maxime.{" "}
@@ -15,12 +18,12 @@ const grid = () => {
         <div className="h-16 bg-blue-500 xms:h-auto xms:aspect-square "></div>
         <div className="h-16 bg-blue-500 xms:h-auto xms:aspect-square "></div>
         <div className="h-16 bg-blue-500 xms:h-auto xms:aspect-square "></div>
+        <div className="h-16 bg-blue-500 xms:h-auto xms:aspect-square md:col-start-2 "></div>
         <div className="h-16 bg-blue-500 xms:h-auto xms:aspect-square "></div>
         <div className="h-16 bg-blue-500 xms:h-auto xms:aspect-square "></div>
         <div className="h-16 bg-blue-500 xms:h-auto xms:aspect-square "></div>
         <div className="h-16 bg-blue-500 xms:h-auto xms:aspect-square "></div>
-        <div className="h-16 bg-blue-500 xms:h-auto xms:aspect-square "></div>
-        <p className="self-center">
+        <p className="self-center md:col-span-2 md:text-center ">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
           suscipit libero laudantium officiis itaque! Ipsa consequatur at
           nostrum amet minima .
