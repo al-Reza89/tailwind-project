560853aa8fd2c1512b02253148ce3eb720557596
al-Reza89 <alialreza98@gmail.com>
2022-09-26 04:32:15 +0600

    left circle complete

--- pages/card.js
+++ pages/card.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { AiOutlineCaretRight } from "react-icons/ai";
 
 const card = () => {
   return (
     <div className="min-h-screen grid place-items-center bg-gray-900">
-      <div className="w-[640px] h-[360px] bg-white ">
+      <div className=" relative w-[640px] h-[360px] bg-white overflow-hidden    ">
+        {/* top left circle */}
+        <div className="w-[200px] -top-16 h-[200px] -left-12 bg-purple-200 absolute rounded-full grid place-items-center ">
+          <AiOutlineCaretRight
+            color="white"
+            className="w-[120px] h-[90px] ml-3 mt-3 "
+          />
+        </div>
+
+        {/* last content */}
         <div className="flex flex-col justify-end h-full p-8 space-y-4 w-2/3 ">
           <p className="uppercase font-semibold text-xs text-pink-600">
             How we are build it
