c1142c00357f706e4dcfd03017613ed091541570
al-Reza89 <alialreza98@gmail.com>
2022-10-09 22:49:44 +0600

    nextjs with scss

---
+++ pages/multilevelDropdown.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/multilevelDropdown.module.scss";
+
+const multilevelDropdown = () => {
+  return <div className={styles.main}>multilevelDropdown</div>;
+};
+
+export default multilevelDropdown;
