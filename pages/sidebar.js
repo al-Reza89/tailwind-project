d133a486e5fbb2cc898ee9fa4afafb299503a885
al-Reza89 <alialreza98@gmail.com>
2022-10-12 04:30:27 +0600

    transition delay css proerty

--- pages/sidebar.js
+++ pages/sidebar.js
@@ -52,6 +52,7 @@ const Sidebar = () => {
                 <>
                   <div>{React.createElement(menu.icon, { size: "20" })}</div>
                   <h2
+                    style={{ transitionDelay: `${i + 3}00ms` }}
                     className={`whitespace-pre duration-500 ${
                       !open && "opacity-0 translate-x-28  "
                     } `}
