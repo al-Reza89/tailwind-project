66c816e2adfd1e34c81cd04293680322c6852491
al-Reza89 <alialreza98@gmail.com>
2022-10-13 02:36:05 +0600

    click function work

--- pages/modal.js
+++ pages/modal.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import ModalC from "../components/modalComponents/ModalC";
 
 const Modal = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const onClose = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
       <div className="p-10 text-center">
         <h1 className="text-3xl mb-5 ">
           Create Custom Modal With tailwind css
         </h1>
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">
+        <button
+          onClick={() => setShowModal(true)}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5"
+        >
           Text Modal
         </button>
         <button className="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none font-medium text-sm rounded-lg px-5 py-2.5 text-center mr-5">
@@ -18,7 +27,7 @@ const Modal = () => {
           From Modal
         </button>
       </div>
-      <ModalC />
+      <ModalC isVisible={showModal} onClose={onClose} />
     </>
   );
 };
